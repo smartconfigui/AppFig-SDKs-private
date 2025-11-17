d2cdf459d0673c29bb5952ef92e303607a2c3a2d
AppFig <appfigdev@gmail.com>
2025-11-17 13:04:54 -0800

    Add SDK build-and-publish workflow

---
+++ ios/AppFig/AppFig/AppFig.swift
@@ -0,0 +1,9 @@
+//
+//  AppFig.swift
+//  AppFig
+//
+//  Created by Alpcan Utku on 11/17/25.
+//
+
+import Foundation
+
---
+++ ios/AppFig/AppFigTests/AppFigTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppFigTests.swift
+//  AppFigTests
+//
+//  Created by Alpcan Utku on 11/17/25.
+//
+
+import Testing
+@testable import AppFig
+
+struct AppFigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
